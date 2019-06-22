064e86ef936d88971fbdff790924715a55db0474
DylanL0ng <dylanhussain08@gmail.com>
2019-06-22 16:20:05 +0100

    transfer mongoDB to a cloud

--- app.js
+++ app.js
@@ -17,7 +17,18 @@ var express = require("express"),
         campgroundRoutes    = require('./routes/campgrounds'),
         authRoutes          = require("./routes/auth")
     
-mongoose.connect('mongodb://localhost/yelp_camp', { useNewUrlParser: true });
+mongoose.connect("mongodb+srv://admin:" + encodeURIComponent('gTg37Rlu%#h4') + "@cluster0-kv9tg.mongodb.net/test?retryWrites=true&w=majority", 
+{ useNewUrlParser: true,
+  useCreateIndex: true
+    
+}).then(() =>{
+    console.log('Connected to DB!');
+}).catch(err => {
+    console.log('ERROR:', err.message);
+});
+
+
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
